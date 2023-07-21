f06dad0e1a296e1ca12c69052dd9aadaadba040e
sarakha63 <sarakha_ludovic@yahoo.fr>
2023-07-21 18:19:36 +0200

    non strict comparison

--- core/class/zwavejs.class.php
+++ core/class/zwavejs.class.php
@@ -824,7 +824,7 @@ class zwavejs extends eqLogic {
 						}
 						if ($data['status'] == 'Dead' && $currentValue == 'Alive') {
 							$action = '<a href="/' . $eqLogic->getLinkToConfiguration() . '">' . __('Equipement', __FILE__) . '</a>';
-							if (version_compare(jeedom::version(),'4.4.0','>')){
+							if (version_compare(jeedom::version(),'4.4.0','>=')){
 								message::add('zwavejs',"L'équipement : " . $eqLogic->getHumanName(true) . ' avec le nodeId : ' . $eqLogic->getLogicalId(). ', vient de passer au statut Dead.', $action,'Dead-'.$eqLogic->getLogicalId(),true,'alerting');
 							} else {
 								message::add('zwavejs',"L'équipement : " . $eqLogic->getHumanName(true) . ' avec le nodeId : ' . $eqLogic->getLogicalId(). ', vient de passer au statut Dead.', $action,'Dead-'.$eqLogic->getLogicalId(),true);
@@ -832,7 +832,7 @@ class zwavejs extends eqLogic {
 						}
 						if ($data['status'] == 'Alive' && $currentValue == 'Dead') {
 							$action = '<a href="/' . $eqLogic->getLinkToConfiguration() . '">' . __('Equipement', __FILE__) . '</a>';
-							if (version_compare(jeedom::version(),'4.4.0','>')){
+							if (version_compare(jeedom::version(),'4.4.0','>=')){
 								message::add('zwavejs',"L'équipement : " . $eqLogic->getHumanName(true) . ' avec le nodeId : ' . $eqLogic->getLogicalId(). ', vient de passer au statut Alive.', $action,'Alive-'.$eqLogic->getLogicalId(),true,'alertingReturnBack');
 							} else {
 								message::add('zwavejs',"L'équipement : " . $eqLogic->getHumanName(true) . ' avec le nodeId : ' . $eqLogic->getLogicalId(). ', vient de passer au statut Alive.', $action,'Alive-'.$eqLogic->getLogicalId(),true);
