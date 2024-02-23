6192dde97f50dbfe05915a49d686a62b11190b8d
sarakha63 <sarakha_ludovic@yahoo.fr>
2024-02-23 13:05:31 +0100

    donc do check if daemon nok

--- core/class/zwavejs.class.php
+++ core/class/zwavejs.class.php
@@ -123,6 +123,10 @@ class zwavejs extends eqLogic {
 	}
 	
 	public static function cronHourly() {
+		$deamon_info = self::deamon_info();
+		if ($deamon_info['state'] == 'ok') {
+			return;
+		}
 		self::getNodes('health');
 	}
 
