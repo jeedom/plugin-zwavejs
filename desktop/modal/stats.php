d8063e494f4438b8abf6f3b2043dbfc02aa3979d
Mips2648 <mips2648@gmail.com>
2026-03-01 08:00:22 +0100

    Fix minor issue on the statistics page

--- desktop/modal/stats.php
+++ desktop/modal/stats.php
@@ -39,8 +39,7 @@ if (!isConnect('admin')) {
 			<?php
 			$eqLogics = eqLogic::byType('zwavejs');
 			foreach ($eqLogics as $eqLogic) {
-				$nameTd = '<td><img src="' . $eqLogic->getImage() . '" height="40"/> <a href="index.php?v=d&p=zwavejs&m=zwavejs&id=' . $eqLogic->getId() . '">' . $eqLogic->getHumanName(true) .  '</a></td>';
-				$healthPage .= '<td><span class="label label-info" style="font-size : 1em;">' . $values['endpointsCount'] . '</span></td>';
+				$nameTd = '<td><img src="' . $eqLogic->getImage() . '" height="40"/> <a href="index.php?v=d&p=zwavejs&m=zwavejs&id=' . $eqLogic->getId() . '">' . $eqLogic->getHumanName(true) . '</a></td>';
 				$nodeId = $eqLogic->getLogicalId();
 				echo '<tr>';
 				echo '<td><span class="label label-primary">' . $nodeId . '</span></td>';
