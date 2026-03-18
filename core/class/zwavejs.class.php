0b071535be24fa599878e72cbd7da9decdace84a
Salvialf <jeuxalf@live.fr>
2026-03-18 17:09:37 +0100

    remove not needed anymore dependancy_info

--- core/class/zwavejs.class.php
+++ core/class/zwavejs.class.php
@@ -302,23 +302,6 @@ class zwavejs extends eqLogic {
 		return $return;
 	}
 
-	public static function dependancy_info() {
-		if (config::byKey('zwavejs::mode', 'zwavejs') == 'distant') {
-			$return = array();
-			$return['state'] = 'ok';
-			return $return;
-		}
-		$return = array();
-		$return['progress_file'] = jeedom::getTmpFolder(__CLASS__) . '/dependance';
-		$return['state'] = 'ok';
-		if (config::byKey('lastDependancyInstallTime', __CLASS__) == '') {
-			$return['state'] = 'nok';
-		} else if (!file_exists(__DIR__ . '/../../resources/zwave-js-ui/node_modules')) {
-			$return['state'] = 'nok';
-		}
-		return $return;
-	}
-
 	public static function deamon_info() {
 		if (config::byKey('zwavejs::mode', 'zwavejs') == 'distant') {
 			$return = array();
